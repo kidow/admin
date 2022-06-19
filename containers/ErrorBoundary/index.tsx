d11959d1bfb4ae5609450d24ceda6293fad719e6
kidow <wcgo2ling@gmail.com>
2022-06-20 00:26:32 +0900

    ErrorBoundary

---
+++ containers/ErrorBoundary/index.tsx
@@ -0,0 +1,38 @@
+import { Component } from 'react'
+import type { ErrorInfo } from 'react'
+
+interface Error {
+  stack?: string
+}
+interface Props extends ReactProps {}
+interface State {
+  hasError: boolean
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.log('componentDidCatch error: ')
+    console.dir(error)
+    console.log('componentDidCatch errorInfo: ')
+    console.dir(errorInfo)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>
+    }
+
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,13 @@
 import 'styles/globals.css'
-import App from 'next/app'
-import { ErrorInfo } from 'react'
+import type { AppProps } from 'next/app'
+import { ErrorBoundary } from 'containers'
 
-interface Props {}
-interface State {
-  hasError: boolean
-}
-
-class MyApp extends App<Props, {}, State> {
-  state = {
-    hasError: false
-  }
-  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    if (error) this.setState({ hasError: true })
-  }
-  static getDerivedStateFromError() {
-    return { hasError: true }
-  }
-  render() {
-    const {} = this.state
-    const { Component, pageProps } = this.props
-    return <Component {...pageProps} />
-  }
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  )
 }
 
 export default MyApp
