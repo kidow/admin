a7e32a093e6b0828a597413af33c0065c92b92a4
kidow <wcgo2ling@gmail.com>
2022-06-20 23:42:16 +0900

    SEO component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
+import { SEO } from 'components'
 import type { NextPage } from 'next'
 
 interface State {}
 
 const HomePage: NextPage = () => {
   return (
-    <div className="container mx-auto">
-      <div>
-        이 사이트는 실제로 어드민을 만든다는 가정하에 테스트 페이지를 만들어 본
-        것입니다.
+    <>
+      <SEO title="홈" />
+      <div className="container mx-auto">
+        <div>
+          이 사이트는 실제로 어드민을 만든다는 가정하에 테스트 페이지를 만들어
+          본 것입니다.
+        </div>
+        <div>무엇이든 자유롭게 테스트하셔도 됩니다.</div>
       </div>
-      <div>무엇이든 자유롭게 테스트하실 수 있습니다.</div>
-    </div>
+    </>
   )
 }
 
